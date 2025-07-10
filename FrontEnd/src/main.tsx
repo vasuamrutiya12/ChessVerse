632d99832d62ae994027ca0790f22d673b63cd64
VASU_ AMRUTIYA_ <vasuamrutiya12@gamil.com>
2025-07-10 19:13:25 +0530

    Update leaderboard

--- FrontEnd/src/main.tsx
+++ FrontEnd/src/main.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
 
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
@@ -12,5 +12,5 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
       <App />
     </GoogleOAuthProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
